2db25fb2edc948da8fe9af49e48dc50185122504
naoto-ogawa <becausethespiderspiedher@gmail.com>
2017-12-02 20:56:24 +0900

    add server port preference

--- src/test/xprotocol.lua
+++ src/test/xprotocol.lua
@@ -6,34 +6,40 @@ function xproto.init ()
   packet_cnt = 0
 end
 
+-- Preferences
+local p = xproto.prefs
+
+p.server_port = Pref.uint ("server port", 8000, "server port number") -- TODO 33060 should be default
+
+
+-- Fields
 local f = xproto.fields
 
 -- f.direction = ProtoField.bytes  ( "XProtocol.direction" , "Direction" )
-f.size      = ProtoField.bytes  ( "XProtocol.size"      , "Size"      )
-f.tipe      = ProtoField.bytes  ( "XProtocol.type"      , "Type"      )
-f.payload   = ProtoField.bytes  ( "XProtocol.payload"   , "Payload"   )
+f.size      = ProtoField.bytes  ("XProtocol.size"      , "Size"   )
+f.tipe      = ProtoField.bytes  ("XProtocol.type"      , "Type"   )
+f.payload   = ProtoField.bytes  ("XProtocol.payload"   , "Payload")
 
-function xproto.dissector (buf, pkt, tree) -- testy vertual buffer
-  pkt.cols.protocol = "XPROTO"
+function xproto.dissector (tvb, pinfo, tree) -- testy vertual tvbfer
+  pinfo.cols.protocol = "XPROTO"
   packet_cnt = packet_cnt + 1
-  info("xproto.dissector, packet_cnt=" .. packet_cnt);
+  -- info("xproto.dissector, packet_cnt=" .. packet_cnt);
 
-  local subtree = tree:add (xproto, buf())
+  local subtree = tree:add (xproto, tvb())
 
-
-  subtree:append_text (" client -> server " )
-  
+  local direction = (pinfo.src_port == p.server_port) and true or false                        -- TODO port should be retrieve from user pref.
+  subtree:append_text (direction and " server -> client " or " client -> server ")
 
   -- size
-  local msg_size = buf(0, 4)
+  local msg_size = tvb(0, 4)
   subtree:add (f.size, msg_size)
   -- type
-  local msg_type = buf(4, 1)
+  local msg_type = tvb(4, 1)
   subtree:add (f.tipe, msg_type)
   -- payload
 
 
-  info("msg_size=" .. msg_size) 
+  -- info("msg_size=" .. msg_size) 
 end
 
 DissectorTable.get("tcp.port"):add(8000,xproto)
