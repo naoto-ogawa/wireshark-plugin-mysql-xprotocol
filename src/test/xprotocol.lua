7e68495ca6f5c2a334f1e73c29dbbd7794bbaa48
naoto-ogawa <becausethespiderspiedher@gmail.com>
2017-12-03 16:23:23 +0900

    check lenght when getting data from tvb and get multiple messages.

--- src/test/xprotocol.lua
+++ src/test/xprotocol.lua
@@ -23,15 +23,28 @@ f.payload   = ProtoField.bytes  ("XProtocol.payload"   , "Payload" )
 
 function getMessageParts (offset, tvb)
   -- size
-  local msg_size = tvb(offset, 4)
-  local payload_len = msg_size :le_int() - 1
-  offset = offset + 4
+  local msg_size
+  local payload_len
+  if offset + 4 <= tvb:len() then
+    msg_size = tvb(offset, 4)
+    payload_len = msg_size :le_int() - 1
+    offset = offset + 4
+  else
+    msg_size    = nil
+    payload_len = nil
+  end
   -- type
-  local msg_type = tvb(offset, 1)
-  local msg_type_num = msg_type :int() 
-  offset = offset + 1
+  local msg_type
+  local msg_type_num
+  if offset + 1 <= tvb:len() then
+    msg_type     = tvb(offset, 1)
+    msg_type_num = msg_type :int() 
+    offset = offset + 1
+  else
+    msg_type     = nil 
+    msg_type_num = nil 
+  end
   -- payload
-  info (string.format("tvb_len=%d, offset=%d, payload_len=%d", tvb:len(), offset, payload_len))
   local msg_payload = (offset + payload_len <= tvb:len()) and tvb(offset, payload_len) or nil
   offset = offset + payload_len
   return offset, msg_size, payload_len, msg_type, msg_type_num, msg_payload
@@ -45,30 +58,21 @@ end
 function xproto.dissector (tvb, pinfo, tree) -- testy vertual tvbfer
   pinfo.cols.protocol = "XPROTO"
   packet_cnt = packet_cnt + 1
-  -- info("xproto.dissector, packet_cnt=" .. packet_cnt);
 
   local subtree = tree:add (xproto, tvb())
 
-  local direction = (pinfo.src_port == p.server_port) and true or false                        -- TODO port should be retrieve from user pref.
+  local direction = (pinfo.src_port == p.server_port) and true or false
   subtree:append_text (direction and " server -> client " or " client -> server ")
 
   local offset = 0
 
-  -- 1st message
-  messages = subtree:add (f.message, tvb(offset,5)) 
-  
-  offset, msg_size, payload_len, msg_type, msg_type_num, msg_payload = getMessageParts (offset, tvb)
-  messages:add (f.size, msg_size) :append_text (string.format(" msg_len (%d) : payload_len (%d)", payload_len+1, payload_len))
-  messages:add (f.tipe, msg_type) :append_text (string.format(" (%d)", msg_type_num))
-  if msg_payload then messages:add (f.payload, msg_payload) end
-
-  -- 2nd message
-  messages = subtree:add (f.message, tvb(offset,5)) 
-  
-  offset, msg_size, payload_len, msg_type, msg_type_num, msg_payload = getMessageParts (offset, tvb)
-  messages:add (f.size, msg_size) :append_text (string.format(" msg_len (%d) : payload_len (%d)", payload_len+1, payload_len))
-  messages:add (f.tipe, msg_type) :append_text (string.format(" (%d)", msg_type_num))
-  if msg_payload then messages:add (f.payload, msg_payload) end
+  while offset < tvb:len() do
+    messages = subtree:add (f.message, tvb(offset,5)) 
+    offset, msg_size, payload_len, msg_type, msg_type_num, msg_payload = getMessageParts (offset, tvb)
+    messages:add (f.size, msg_size) :append_text (string.format(" msg_len (%d) : payload_len (%d)", payload_len+1, payload_len))
+    messages:add (f.tipe, msg_type) :append_text (string.format(" (%d)", msg_type_num))
+    if msg_payload then messages:add (f.payload, msg_payload) end
+  end
 
 end
 
