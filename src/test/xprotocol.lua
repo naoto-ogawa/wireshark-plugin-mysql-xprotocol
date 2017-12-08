24fbf143f89f75854a7a3bdcd11c772e18203980
naoto-ogawa <becausethespiderspiedher@gmail.com>
2017-12-08 18:47:29 +0900

    decode key (wire_type and tag_no) decode multi-byte data by handing MSB

--- src/test/xprotocol.lua
+++ src/test/xprotocol.lua
@@ -1,3 +1,5 @@
+require "alien"
+
 local xproto = Proto ("XProtocol", "X Protocol Dissector");
 
 local packet_cnt
@@ -111,11 +113,6 @@ function getMessageParts (offset, tvb)
   return offset, msg_size, payload_len, msg_type, msg_type_num, msg_payload
 end
 
--- 0123456789  ---- len = 10
--- ++++        0 4 
---     +       4 1
---      +++++  5 5
-
 function xproto.dissector (tvb, pinfo, tree) -- tvb = testy vertual tvbfer
   pinfo.cols.protocol = "XPROTO"
   packet_cnt = packet_cnt + 1
@@ -143,12 +140,81 @@ function xproto.dissector (tvb, pinfo, tree) -- tvb = testy vertual tvbfer
         :append_text (tostring(direction and servermessagetype[msg_type_num] or clientmessagetype[msg_type_num]))
     end
     if msg_payload then 
-      messages:add (f.payload, msg_payload) 
+      payload = messages:add (f.payload, msg_payload) 
+      payload :add (string.format(" length (%d)", msg_payload:len()))
+      if msg_payload :len() > 0 then
+        po = 0
+        while po < msg_payload :len() do
+          wiretype , tagno, po = getwiretag(po, msg_payload)
+          if (wiretype == 0) then
+            val, acc, po = getLengthVal(po, msg_payload)
+            payload :add (string.format("[(%d)] wiret_type (%d), tag_no (%d) value (%d) acc (%d)", po, wiretype, tagno, val, acc))
+
+          elseif (wiretype == 2) then
+            le, acc, po = getLengthVal(po, msg_payload)
+            va = msg_payload(po, acc) : string()
+            po = po + acc
+            payload :add (string.format("[(%d)] wiret_type (%d), tag_no (%d) length (%d) acc(%d) value (%s)", po, wiretype, tagno, le, acc, va))
+          end
+        end
+      end
     end
     updateState(msg_size, payload_len, msg_type, msg_type_num, msg_payload)
   end
 end
 
+function getLengthVal (offset, tvb) 
+  b = tvb(offset, 1)
+  offset = offset + 1
+  acc1, base = getnumber(0, 1, b)
+  -- info(string.format("acc (%d), (%d), base (%d), bitfield (%d)", b:uint(), acc1, base, b:bitfield(0,1)))
+  -- info( b:bitfield(0,1) == 1)
+  while b:bitfield(0,1) == 1 do
+    b = tvb(offset, 1)
+    offset = offset + 1
+    acc1, base = getnumber(acc1, base, b)
+    -- info(string.format("acc2 (%d), base (%d)", acc1, base))
+  end
+  return b:uint(), acc1, offset
+end
+
+function getnumber(acc, base, val) 
+  for i=7, 1, -1 do
+    if val :bitfield(i,1) == 1 then
+      acc = acc + (2^(base-1))
+    end
+    base = base + 1
+  end
+  return acc, base
+end
+
+function getwiretag(offset, tvb) 
+  key  = tvb(offset, 1) :uint()  -- TODO check length
+  wire = bit32.band(key, 7)      -- TODO check value
+  tag  = bit32.rshift(key,3)     -- TODO check value
+  offset = offset + 1
+  return wire, tag, offset
+end
+
+function zigzag(tvb)
+  v = tvb :int()
+  a = bit32.rshift(v,1)
+  b = bit32.band  (v,1)
+  return bit32.bxor(a, b * -1)  -- TODO bit
+end
+
+-- -1
+-- > print(bit32.band(1,127)*2 - 1)
+-- 1
+-- > print(bit32.band(2,127)*2 - 2)
+-- 2
+-- > print(bit32.band(3,127)*2 - 3)
+-- 3
+-- > print(bit32.band(255,127)*2 - 255)
+-- -1
+-- >
+
+-- state management
 function initState()
   state.payload_len  = nil
   state.msg_type_num = nil
@@ -184,5 +250,67 @@ end
 DissectorTable.get("tcp.port"):add(8000,xproto)
 
 
+-- 0a 0000 1010 wire=2, tag=1
+-- cf 1100 1111 
+-- 02 0000 0010  -> 10 1001111
 
+-- 0000   0a cf 02 7b 22 47 4e 50 22 3a 20 38 32 38 2c 20
+--        
+-- 0010   22 5f 69 64 22 3a 20 22 41 42 57 22 2c 20 22 4e
+-- 0020   61 6d 65 22 3a 20 22 41 72 75 62 61 22 2c 20 22
+-- 0030   49 6e 64 65 70 59 65 61 72 22 3a 20 6e 75 6c 6c
+-- 0040   2c 20 22 67 65 6f 67 72 61 70 68 79 22 3a 20 7b
+-- 0050   22 52 65 67 69 6f 6e 22 3a 20 22 43 61 72 69 62
+-- 0060   62 65 61 6e 22 2c 20 22 43 6f 6e 74 69 6e 65 6e
+-- 0070   74 22 3a 20 22 4e 6f 72 74 68 20 41 6d 65 72 69
+-- 0080   63 61 22 2c 20 22 53 75 72 66 61 63 65 41 72 65
+-- 0090   61 22 3a 20 31 39 33 7d 2c 20 22 67 6f 76 65 72
+-- 00a0   6e 6d 65 6e 74 22 3a 20 7b 22 48 65 61 64 4f 66
+-- 00b0   53 74 61 74 65 22 3a 20 22 42 65 61 74 72 69 78
+-- 00c0   22 2c 20 22 47 6f 76 65 72 6e 6d 65 6e 74 46 6f
+-- 00d0   72 6d 22 3a 20 22 4e 6f 6e 6d 65 74 72 6f 70 6f
+-- 00e0   6c 69 74 61 6e 20 54 65 72 72 69 74 6f 72 79 20
+-- 00f0   6f 66 20 54 68 65 20 4e 65 74 68 65 72 6c 61 6e
+-- 0100   64 73 22 7d 2c 20 22 64 65 6d 6f 67 72 61 70 68
+-- 0110   69 63 73 22 3a 20 7b 22 50 6f 70 75 6c 61 74 69
+-- 0120   6f 6e 22 3a 20 31 30 33 30 30 30 2c 20 22 4c 69
+-- 0130   66 65 45 78 70 65 63 74 61 6e 63 79 22 3a 20 37
+-- 0140   38 2e 34 30 30 30 30 31 35 32 35 38 37 38 39 7d
+-- 0150   7d 00
+
+-- VarInt   = 0
+-- Bit64    = 1
+-- LenDelim = 2
+-- Bit32    = 5
+-- 
+--     0-tag(4)-wire(3)
+--
+--  08 0000 1000  wire=0, tag=1      
+--  12 0001 0010  wire=2, tag=2
+--  1a 0001 1010  wire=2, tag=3
+--  22 0010 0010  wire=2, tag=4
+--  2a 0010 1010  wire=2, tag=5
+--  32 0011 0010  wire=2, tag=6
+--  3a 0011 1010  wire=2, tag=7
+--  40 0100 0000  wire=0, tag=8       
+--  48 0100 1000  wire=0, tag=9
+--  50 0101 0000  wire=0, tag=10
+--  58 0101 1000  wire=0, tag=11
+--  60 0110 0000  wire=0, tag=12
+-- 
+--  3f 0011 1111
+--
+--  ff ff ff ff 0f = 11111111 11111111 11111111 11111111 00001111
+--                   0000 0000 1111 1111 1111 1111 1111 1111 1111 1111 
+-- 
+-- 0000  08 07 12 03 64 6f 63 1a  03 64 6f 63 22 0b 63 6f   ....doc. .doc".co
+--       **    **             **  ~~          ** ~~
+-- 0010  75 6e 74 72 79 69 6e 66  6f 2a 0b 63 6f 75 6e 74   untryinf o*.count
+--                                   ** ~~             
+-- 0020  72 79 69 6e 66 6f 32 07  77 6f 72 6c 64 5f 78 3a   ryinfo2. world_x:
+--                         ** ~~                       **
+-- 0030  03 64 65 66 40 3f 48 00  50 ff ff ff ff 0f 58 00   .def@?H. P.....X.
+--       ~~          **    **     **                **
+-- 0040  60 02  
+--       ** 
 
